733d737bf4f36ad3c4aa2e967e16a7bc8b4e3fab
Mohanraj.R <144455467+mohanrkannan@users.noreply.github.com>
2023-09-26 14:08:44 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,12 +1,29 @@
-# Input year
-year = int(input("Enter a year: "))
+def LinearSearchProduct(productList,targetProduct):
 
-# Check if it's a leap year
-if year % 400 == 0:
-    print(year, "is a leap year.")
-elif year % 100 == 0:
-    print(year, "is not a leap year.")
-elif year % 4 == 0:
-    print(year, "is a leap year.")
-else:
-    print(year, "is not a leap year.")
+    indices = []
+
+    for index, product in enumerate(productList):
+
+        if product ==  targetProduct:
+
+            indices.append(index)            
+
+            return indices
+
+        
+
+
+
+products = ["shoes","boot","loafer","shoes","sandals","shoes"]         
+
+target = "shoes"
+
+target2 = "apple"
+
+result = LinearSearchProduct(products,target)
+
+result2 = LinearSearchProduct(products,target2)
+
+print(result) 
+
+print(result2)
--- main.py
+++ main.py
@@ -1,9 +1,41 @@
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
+class Student:
 
-num = int(input("Enter a number: "))
-result = factorial(num)
-print(f"The factorial of {num} is {result}")
+    def __init__(self, name, roll_number, cgpa):
+
+        self.name =name
+
+        self.roll_number = roll_number
+
+        self.cgpa = cgpa
+
+        
+
+def sort_students(student_list):
+
+    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+
+    return sorted_students
+
+
+
+students = [
+
+    Student("Hari", "A123", 7.8),
+
+    Student("srikanth","A124", 8.9),
+
+    Student("Saumya","A125", 9.1),
+
+    Student("Mahidhar","A126", 9.9),
+
+    ]            
+
+
+
+sorted_students = sort_students(students)
+
+
+
+for student in sorted_students:
+
+    print("Name:{}, Roll Number:{}, CGPA:{}".format(student.name,student.roll_number, student.cgpa))
